451dec75fbc2132c9a9600905217672e3eebb3ce
sunilthemaster <sunil_the_master@yahoo.com>
2020-10-23 15:25:33 +0530

    added exlcusions for sonar

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,23 @@
 
 set -e
 
-echo -e "Called"
-
-sonar-scanner \
-    -Dsonar.host.url=${INPUT_HOST} \
-    -Dsonar.login=${INPUT_LOGIN} \
-    -Dsonar.projectKey=${INPUT_PROJECTNAME} \
-    -Dsonar.projectName=${INPUT_PROJECTNAME} \
-    -Dsonar.sources=. \
-    -Dsonar.exclusions="utils/**,violet/Docker/Swarm/60-Maintenance/**,violet/Docker/Swarm/Scripts/AWS-Swarm/**,violet/Docker/Swarm/Scripts/azure/**,violet/Jenkins/AWS/**"
+if [ -n ${INPUT_EXCLUSIONS} ]; then
+{
+    sonar-scanner \
+        -Dsonar.host.url=${INPUT_HOST} \
+        -Dsonar.login=${INPUT_LOGIN} \
+        -Dsonar.projectKey=${INPUT_PROJECTNAME} \
+        -Dsonar.projectName=${INPUT_PROJECTNAME} \
+        -Dsonar.sources=${INPUT_PROJECTBASEDIR} \
+        -Dsonar.exclusions="${INPUT_EXCLUSIONS}"
+}
+else
+{
+    sonar-scanner \
+        -Dsonar.host.url=${INPUT_HOST} \
+        -Dsonar.login=${INPUT_LOGIN} \
+        -Dsonar.projectKey=${INPUT_PROJECTNAME} \
+        -Dsonar.projectName=${INPUT_PROJECTNAME} \
+        -Dsonar.sources${INPUT_PROJECTBASEDIR}
+}
+fi
